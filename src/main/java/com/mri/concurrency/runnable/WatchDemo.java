0660e8e19fc28b3cd05fc9e88690b8731c0009ae
mrinmoybm <eDr45gSc24#>
2023-10-21 09:25:16 +0600

    stopping thread

---
+++ src/main/java/com/mri/concurrency/runnable/WatchDemo.java
@@ -0,0 +1,33 @@
+package com.mri.concurrency.runnable;
+
+import com.mri.concurrency.util.CommonUtil;
+
+public class WatchDemo implements Runnable{
+
+    private boolean running = true;
+    @Override
+    public void run() {
+        while (running) {
+            CommonUtil.printCurrentTime();
+            CommonUtil.sleepOneSecond();
+
+            if (Thread.interrupted()) {
+                System.out.println("Thread is interuppted!");
+                return;
+            }
+        }
+    }
+
+    private void shutDown() {
+        this.running = false;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WatchDemo watch = new WatchDemo();
+        Thread watchThread = new Thread(watch);
+        watchThread.start();
+        Thread.sleep(500);
+        //watchThread.interrupt();
+        watch.shutDown();
+    }
+}
--- src/main/java/com/mri/concurrency/util/CommonUtil.java
+++ src/main/java/com/mri/concurrency/util/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.mri.concurrency.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 public class CommonUtil {
@@ -11,5 +13,11 @@ public class CommonUtil {
         catch (InterruptedException e){
             e.printStackTrace();
         }
+    }
+
+    public static void printCurrentTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:s a");
+        String formattedCurrentTime = LocalDateTime.now().format(formatter);
+        System.out.println(formattedCurrentTime);
     }
 }
