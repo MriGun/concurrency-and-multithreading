412cef1a4c0de43fa07b7dae53ae124db2d0042a
mrinmoybm <eDr45gSc24#>
2023-11-06 09:00:16 +0600

    executor service example

---
+++ src/main/java/com/mri/concurrency/pool/ThreadExecutorsExample.java
@@ -0,0 +1,23 @@
+package com.mri.concurrency.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class ThreadExecutorsExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        IntStream.range(0, 100).forEach(i -> {
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.printf("Thread %s is executing task: %d%n", threadName, i);
+                }
+            };
+            executorService.execute(task);
+        });
+        executorService.shutdown();
+    }
+}
