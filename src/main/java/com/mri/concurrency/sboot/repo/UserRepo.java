4f008cc397dc0a7848eb9746222afd666a2945a1
mrinmoybm <eDr45gSc24#>
2023-11-29 23:24:13 +0600

    completable future

---
+++ src/main/java/com/mri/concurrency/sboot/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.mri.concurrency.sboot.repo;
+
+import com.mri.concurrency.sboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+}
